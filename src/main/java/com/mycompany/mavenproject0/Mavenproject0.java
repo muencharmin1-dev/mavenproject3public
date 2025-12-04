d4013a741a81fe8c1ae05acdf61c41250c5d6b4d
Armin <muencharmin1@gmail.com>
2025-12-04 17:59:36 +0100

    Test-Commit

---
+++ src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject0;
+
+/**
+ *
+ * @author Anwender
+ */
+public class Mavenproject0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("jetzt muss es doch klappen oder");
+        //halloooo
+    }
+}
